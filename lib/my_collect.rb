d0d53e73f97aba7f908335bb02081f0c08ac1fe2
Nataliya Rudenko <n.rudenko@gmail.com>
2019-01-30 22:41:49 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,15 @@
+def my_collect(empty_array)
+  empty_array = []
+end
+ 
+ def my_collect(languages)
+   languages.collect do |languages|
+     languages.upcase
+     yield languages
+   end
+ end
 
+  
 
+   
+   
