05539ecb9c5ebd147c57ecbd999f9360b401d783
Nataliya Rudenko <n.rudenko@gmail.com>
2019-01-30 22:37:36 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,20 @@
+def my_collect(empty_array)
+  empty_array = []
+end
+ 
+ def my_collect(languages)
+   languages.collect do |languages|
+     languages.upcase
+     yield languages.upcase
+   end
+ end
+def my_collect(array)
+  my_collect(array) do |name|
+    name.split("").first 
+  end
+end
 
+  
 
+   
+   
